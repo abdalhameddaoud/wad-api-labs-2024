8e6a4c9da63c7d0910b9028044324474fe6989c0
Your Name <you@example.com>
2024-04-23 10:53:33 +0100

    Mongo lab Exercises done

--- tasky-api/index.js
+++ tasky-api/index.js
@@ -3,6 +3,8 @@ import express from 'express';
 import tasksRouter from './api/tasks';
 import './db';
 import usersRouter from './api/users';
+// other imports
+import cors from 'cors';
 
 dotenv.config();
 
@@ -18,6 +20,9 @@ const errHandler = (err, req, res, next) => {
 const app = express();
 
 const port = process.env.PORT;
+
+// Enable CORS for all requests
+app.use(cors());
 
 app.use(express.json());
 
